76b61454f661f8c590172244559dbc82a10f536f
Skye <skye.grossman@gmail.com>
2021-04-20 06:12:34 -1000

    first commit

---
+++ thistimeits.js
@@ -0,0 +1,11 @@
+function displayIfChildIsAbleToRideTheRollerCoaster(){
+for(var i = 30; i <=52; i ++){
+    if(i >= 52){
+        console.log('Get on that ride kiddo!');
+    } else {
+        console.log('Sorry kiddo, maybe next year.')
+    }
+}
+}
+displayIfChildIsAbleToRideTheRollerCoaster()
+
